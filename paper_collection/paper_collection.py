12330ca74d2632b52bbc99376a8804a373c96abe
hi-the-swan <jason.portenoy@gmail.com>
2020-03-04 18:57:40 -0800

    started classes for Paper and PaperCollection

--- paper_collection/paper_collection.py
+++ paper_collection/paper_collection.py
@@ -18,17 +18,69 @@ logger = root_logger.getChild(__name__)
 
 class Paper:
 
-    """Docstring for Paper. """
+    """A single article, from a single data set."""
 
-    def __init__(self):
+    def __init__(self,
+                 dataset=None,
+                 dataset_version=None,
+                 paper_id=None,
+                 title=None,
+                 display_title=None,
+                 doi=None,
+                 url=None,
+                 pub_date=None,
+                 year=None):
         """TODO: to be defined1. """
+
+        self.dataset = dataset
+        self.dataset_version = dataset_version
+        self.paper_id = paper_id
+        self.title = title
+        self.display_title = display_title
+        self.doi = doi
+        self.url = url
+        self.pub_date = pub_date
+        self.year = year
+
+        self.authors = []
+        self.venue = None
+
+        if (not self.display_title) and (self.title):
+            self.display_title = self.title.title()
+
+        if (not self.url) and (self.doi):
+            self.url = "https://doi.org/{}".format(self.doi)
+
+    def __repr__(self):
+        return "Paper(paper_id: {})".format(self.paper_id)
 
 class PaperCollection:
 
-    """Docstring for PaperCollection. """
+    """A collection of Papers"""
 
-    def __init__(self):
-        """TODO: to be defined1. """
+    def __init__(self, 
+                 papers=None, 
+                 description=None,
+                 citations=None):
+        """
+        papers: list of Paper objects
+        description: (str) description of this collection
+        citations: list of tuples (citing_id, cited_id)
+        """
+
+        self.papers = papers
+        if self.papers is None:
+            self.papers = list()
+        self.description = description
+        self.citations = citations
+        if self.citations is None:
+            self.citations = list()
+
+    def __repr__(self):
+        return "PaperCollection({})".format(self.description)
+
+    def __len__(self):
+        return len(self.papers)
 
         
         
--- tests/test_paper_collection.py
+++ tests/test_paper_collection.py
@@ -7,15 +7,42 @@ import unittest
 
 from paper_collection import paper_collection
 
+import pandas as pd
+import numpy as np
+
 
 class TestPaper_collection(unittest.TestCase):
     """Tests for `paper_collection` package."""
 
     def setUp(self):
         """Set up test fixtures, if any."""
+        self.df_papers = pd.read_csv('tests/jw_papers_mag2019.tsv', sep='\t')
+        self.df_citations = pd.read_csv('tests/jw_citations_mag2019.tsv', sep='\t')
 
     def tearDown(self):
         """Tear down test fixtures, if any."""
 
-    def test_000_something(self):
-        """Test something."""
+    def load_paper(self, prow):
+        return paper_collection.Paper(dataset='mag',
+                                      dataset_version='mag-2019-11-22',
+                                      paper_id=prow.PaperId,
+                                      title=prow.PaperTitle,
+                                      display_title=prow.OriginalTitle,
+                                      doi=prow.Doi,
+                                      pub_date=prow.Date,
+                                      year=prow.Year)
+
+    def test_000_single_paper(self):
+        """Load a single paper"""
+        prow = self.df_papers.iloc[0]
+        p = self.load_paper(prow)
+        assert p.display_title is not None
+        assert len(p.display_title)
+
+    def test_001_collection(self):
+        """Load a collection"""
+        coll = paper_collection.PaperCollection(description="Paper Collection")
+        for _, prow in self.df_papers.iterrows():
+            p = self.load_paper(prow)
+            coll.papers.append(p)
+        assert len(coll) == 45
